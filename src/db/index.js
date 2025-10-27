2ad7d95e1908ac7aedc92088371e63c479925dbf
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-10-27 21:18:03 +0530

    setup database connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n Connected to the database successfully to ${connectionInstance.
+            connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to the database", error);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB;
