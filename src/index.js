5c8868f342088c0f6ab62038bd1da0c209ad8cfd
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-10-28 21:33:09 +0530

    Add asyncHandler, ApiError, and ApiResponse utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
 
 
-connectDB(); 
+connectDB().then(
+    () => {
+        app.listen(process.env.PORT|| 8000, () => {
+            console.log(`\n Server is running on port ${process.env.PORT}`);
+        });
+        app.on("error", (err) => {
+        console.error(" Server error:", err);
+        process.exit(1);
+    });
+    }
+).catch((err) => {
+    console.error("Failed to connect to the database", err);
+});
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,10 @@
+class apiResponse{
+    constructor(statusCode, message, data = null){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode < 400;
+    }
+}
+
+export {apiResponse};
