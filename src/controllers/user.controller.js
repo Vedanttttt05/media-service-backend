4eacd79a7d013dac850cef5188f9af8d2c5ed7be
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-12-02 03:53:23 +0530

    add  subscriber / subscribed logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -311,6 +311,66 @@ const updateCoverImage  = asyncHandler(async(req,res) => {
 
 })
 
+const getUserChannelProfile = asyncHandler(async(req , res ) => {
+
+  const {username} = req.params
+  
+  if (!username?.trim()){
+    throw new apiError (400 , "username is required")
+  }
+  const channel = await User.aggregate([
+    {
+      $match : { username : username?.toLowerCase() }
+    },
+    {
+    $lookup : {
+      from : "subscriptions",
+      localField : "_id",
+      foreignField : "channel",
+      as : "subscribers"
+    }},
+    {
+      $lookup : {
+        from : "subscriptions",
+        localfield : "_id",
+        foreignField : "subscriber",
+        as : "subscribedChannels"
+      }
+    }
+    ,
+    {
+      $addFields:{
+        subscribersCount : { $size : "$subscribers" },
+        channelsSubscribedToCount : { $size : "$subscribedChannels"},
+        isSubscribedToChannel : {
+           $cond : {
+            if : {$in : [ req.user?._id , "$subscribers.subscriber" ] } ,
+            then : true ,
+            else : false
+           }
+        }
+      }
+    },
+    {
+      $project : {
+        fullName : 1 ,
+        username : 1 ,
+        subscribersCount : 1 ,
+        channelsSubscribedToCount : 1 ,
+        isSubscribedToChannel : 1 ,
+        avatar : 1 ,
+        coverImage : 1,
+        email : 1 ,
+        createdAt : 1
+      }
+    }
+  ])
+  if (!channel?.length){
+    throw new apiError (404 , "Channel not found")
+  }
+  return res.status(200).json (new apiResponse (200 , channel[0] , "Channel profile fetched successfully"))
+
+})
 
 
 
@@ -318,4 +378,6 @@ export { registerUser , loginUser ,
    logOutUser , refreshAccessToken  ,
     changePassword , getCurrentUser ,
      updateDetails , updateAvatar ,
-      updateCoverImage};
+      updateCoverImage , getUserChannelProfile};
+
+  
