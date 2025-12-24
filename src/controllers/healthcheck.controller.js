33c16e872af7d8809078a4e5ed35f499ef2436cd
Vedanttttt05 <vedantpatil05052004@gmail.com>
2025-12-25 02:13:32 +0530

    fix(likes): support polymorphic likes for video/comment/tweet

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,5 +1,4 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
-import apiError  from "../utils/apiError.js"
 import  apiResponse  from "../utils/apiResponse.js"
 
 const healthcheck = asyncHandler(async (req, res) => {
--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,12 +1,26 @@
 import mongoose , {Schema} from "mongoose";
 
 const likeSchema = new Schema({
-    video : { type: Schema.Types.ObjectId, ref: "Video", required: true },
-    comment : { type: Schema.Types.ObjectId, ref: "Comment" },
-    tweet : { type: Schema.Types.ObjectId, ref: "Tweet" },
+    video : { type: Schema.Types.ObjectId, ref: "Video", default: null },
+    comment : { type: Schema.Types.ObjectId, ref: "Comment"  , default: null },
+    tweet : { type: Schema.Types.ObjectId, ref: "Tweet"  , default: null },
     likedBy : { type: Schema.Types.ObjectId, ref: "User", required: true },
     
 }
 , { timestamps: true } );
+
+likeSchema.pre("validate", function (next) {
+  const targets = [this.video, this.comment, this.tweet].filter(Boolean);
+
+  if (targets.length === 0) {
+    return next(new Error("Like must belong to a video, comment, or tweet"));
+  }
+
+  if (targets.length > 1) {
+    return next(new Error("Like can belong to only one target"));
+  }
+
+  next();
+});
 
 export const Like = mongoose.model("Like", likeSchema);
