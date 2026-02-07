484867ed22d0bb7e497ce9aa723ab31e4dc34804
Vedanttttt05 <vedantpatil05052004@gmail.com>
2026-02-07 23:13:22 +0530

    fix apiError and apiResponse case sensitive imports

--- src/middlewares/ownership.middleware.js
+++ src/middlewares/ownership.middleware.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-import ApiError from "../utils/ApiError.js"
+import ApiError from "../utils/apiError.js"
 
 export const verifyOwnership = (Model) =>
     asyncHandler(async (req, res, next) => {
